f26b33ff6890c6377120b3f661b53dbae7e00b16
ysvet <44068276+ysvet@users.noreply.github.com>
2019-01-09 19:48:54 +0200

    starship-page refactored, withRouter

--- src/components/pages/starships-page.js
+++ src/components/pages/starships-page.js
@@ -1,24 +1,13 @@
-import React, { Component } from 'react';
-import { StarshipDetails, StarshipList } from '../sw-components';
-import Row from '../row';
+import React from 'react';
+import { StarshipList } from '../sw-components';
+import { withRouter } from 'react-router-dom';
 
-export default class StarshipsPage extends Component {
+const StarshipsPage = ({ history }) => {
+ return (
+      <StarshipList onItemSelected={(itemId) => {
+        history.push(`/starships/${itemId}`);
+      }} />
+ );
+};
 
-  state = {
-    selectedItem: null
-  };
-
-  onItemSelected = (selectedItem) => {
-    this.setState({ selectedItem });
-  };
-
-  render() {
-    const { selectedItem } = this.state;
-
-    return (
-      <Row
-        left={<StarshipList onItemSelected={this.onItemSelected} />}
-        right={<StarshipDetails itemId={selectedItem} />} />
-    );
-  }
-}
+export default withRouter(StarshipsPage);
